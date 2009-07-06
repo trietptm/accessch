f35fb889a75f0f422f9fdb423864653d729bdda0
Andrey.Sobko <Andrey.Sobko@202ed0b6-6a49-11de-80b8-55702d16276a>
2009-07-06 16:26:40 +0000

    init

---
+++ drv/main.h
@@ -0,0 +1,8 @@
+#pragma warning(error:4100)     //  Enable-Unreferenced formal parameter
+#pragma warning(error:4101)     //  Enable-Unreferenced local variable
+#pragma warning(error:4061)     //  Eenable-missing enumeration in switch statement
+#pragma warning(error:4505)     //  Enable-identify dead functions
+
+#include <fltKernel.h>
+#define NTSTRSAFE_LIB
+#include <ntstrsafe.h>
