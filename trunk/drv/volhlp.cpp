31d73c2854f3f6dd5b68aebbbc513f0bcf8384c1
andrey.sobko@gmail.com <andrey.sobko@gmail.com@202ed0b6-6a49-11de-80b8-55702d16276a>
2010-07-26 13:25:44 +0000

    common header (devctrl) usage

--- trunk/drv/volhlp.cpp
+++ trunk/drv/volhlp.cpp
@@ -1,9 +1,7 @@
 #include "volhlp.h"
+#include "../../devctrl/inc/devctrlex.h"
 
-// {E636CD65-4021-4d20-97EC-5AE23189566B}
-DEFINE_GUID( GET_MEDIA_SERIAL_NUMBER_GUID, 
-            0xe636cd65,
-            0x4021, 0x4d20, 0x97, 0xec, 0x5a, 0xe2, 0x31, 0x89, 0x56, 0x6b );
+
 NTSTATUS
 QueryDeviceProperty (
     __in PDEVICE_OBJECT Device,
