b65fd45f02244a21fac896de054019270b904230
Andrey.Sobko <andrey.sobko@gmail.com>
2010-08-25 10:35:30 +0000

    убрал лишний класс, теперь все контекстные классы являются наследниками EventData

--- drv/fileflt.h
+++ drv/fileflt.h
@@ -1,53 +1,15 @@
 #ifndef __fileflt_h
 #define __fileflt_h
 
-class FileInterceptorContext : public InterceptorContext
+class FileInterceptorContext : public EventData
 {
-private:
-    // intercepted data
-    PFLT_CALLBACK_DATA          m_Data;
-    PCFLT_RELATED_OBJECTS       m_FltObjects;
-    OperationPoint              m_OperationType;
-
-    // service field
-    PSTREAM_CONTEXT             m_StreamContext;
-    LONG                        m_StreamFlagsTemp;
-    LONG                        m_CacheSyncronizer;
-
-    // data access
-    HANDLE                      m_Section;
-    PVOID                       m_SectionObject;
-    PVOID                       m_MappedBase;
-
-    // queryed parameters
-    HANDLE                      m_RequestorProcessId;
-    HANDLE                      m_RequestorThreadId;
-    PINSTANCE_CONTEXT           m_InstanceContext;
-    PFLT_FILE_NAME_INFORMATION  m_FileNameInfo;
-    PSID                        m_Sid;
-    LUID                        m_Luid;
-
-    ACCESS_MASK                 m_DesiredAccess;
-    ULONG                       m_CreateOptions;
-    ULONG                       m_CreateMode;
-    
-private:
-    __checkReturn
-    NTSTATUS
-    CheckAccessToStreamContext (
-        );
-
-    __checkReturn
-     NTSTATUS
-     CreateSectionForData (
-        __deref_out PHANDLE Section,
-        __out PLARGE_INTEGER Size
-        );
-
 public:
     FileInterceptorContext (
         __in PFLT_CALLBACK_DATA Data,
         __in PCFLT_RELATED_OBJECTS FltObjects,
+        __in Interceptors InterceptorId,
+        __in DriverOperationId Major,
+        __in ULONG Minor,
         __in OperationPoint OperationType
         );
 
@@ -72,6 +34,46 @@ public:
 
     void
     SetCache1();
+
+private:
+    __checkReturn
+    NTSTATUS
+    CheckAccessToStreamContext (
+        );
+
+    __checkReturn
+    NTSTATUS
+    CreateSectionForData (
+        __deref_out PHANDLE Section,
+        __out PLARGE_INTEGER Size
+        );
+
+private:
+    // intercepted data
+    PFLT_CALLBACK_DATA          m_Data;
+    PCFLT_RELATED_OBJECTS       m_FltObjects;
+
+    // service field
+    PSTREAM_CONTEXT             m_StreamContext;
+    LONG                        m_StreamFlagsTemp;
+    LONG                        m_CacheSyncronizer;
+
+    // data access
+    HANDLE                      m_Section;
+    PVOID                       m_SectionObject;
+    PVOID                       m_MappedBase;
+
+    // queryed parameters
+    HANDLE                      m_RequestorProcessId;
+    HANDLE                      m_RequestorThreadId;
+    PINSTANCE_CONTEXT           m_InstanceContext;
+    PFLT_FILE_NAME_INFORMATION  m_FileNameInfo;
+    PSID                        m_Sid;
+    LUID                        m_Luid;
+
+    ACCESS_MASK                 m_DesiredAccess;
+    ULONG                       m_CreateOptions;
+    ULONG                       m_CreateMode;
 };
 
 #endif // __fileflt_h
