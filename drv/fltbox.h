8eeb9a4e25fa2e07e3c2491ca222255f1565ed12
andrey.sobko <andrey.sobko@202ed0b6-6a49-11de-80b8-55702d16276a>
2010-11-30 12:55:10 +0000

    доработки блоков правил

--- drv/fltbox.h
+++ drv/fltbox.h
@@ -4,7 +4,13 @@
 class FilterBox
 {
 public:
-    FilterBox();
+    static ULONG    m_AllocTag;
+
+public:
+    FilterBox (
+        __in LPGUID Guid
+        );
+
     ~FilterBox();
 
     __checkReturn
@@ -14,10 +20,34 @@ public:
     void
     Release();
 
+    __checkReturn
+    NTSTATUS
+    AddParams (
+        __in ULONG ParamsCount,
+        __in PPARAM_ENTRY Params,
+        __out PULONG Position
+        );
+
+    NTSTATUS
+    MatchEvent (
+        __in EventData *Event,
+        __in PRTL_BITMAP Affecting
+        );
+
+public:
+    LIST_ENTRY      m_List;
+    LONG            m_RefCount;
+    GUID            m_Guid;
+
 private:
-    EX_RUNDOWN_REF      m_Ref;
+    ULONG           m_NextFreePosition;
+
+    LIST_ENTRY      m_Items;
 };
 
+#define PFilterBox FilterBox*
+
+//////////////////////////////////////////////////////////////////////////
 
 class FilterBoxList
 {
@@ -28,11 +58,31 @@ public:
     __checkReturn
     NTSTATUS
     GetOrCreateBox (
-        LPGUID Guid
+        __in LPGUID Guid,
+        __deref_out_opt PFilterBox* FltBox
+        );
+
+    FilterBox*
+    LookupBox (
+        __in LPGUID Guid
         );
 
 private:
+    FilterBox*
+    CreateNewp (
+        __in LPGUID Guid
+        );
 
+    FilterBox*
+    LookupBoxp (
+        __in LPGUID Guid
+        );
+
+private:
     EX_PUSH_LOCK    m_AccessLock;
+    LIST_ENTRY      m_List;
 };
+
+#define PFilterBoxList FilterBoxList*
+
 #endif // __fltbox_h
